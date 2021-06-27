2a85e315f9ed806b2d8490edcbc4ae3ec9dbd270
Tom Dalby <thomasjackdalby@gmail.com>
2021-06-27 22:38:16 +0100

    Problem 15 (quick!)

--- 015.py
+++ 015.py
@@ -17,4 +17,19 @@ from common import check
 PROBLEM_NUMBER = 15
 ANSWER_HASH = "928f3957168ac592c4215dcd04e0b678"
 
-check(None, PROBLEM_NUMBER, ANSWER_HASH)
+cache = {}
+
+def calculate(x, y):
+    if (x, y) == (1, 1):
+        return 2
+    elif (x, y) in cache:
+        return cache[(x, y)]
+    else:
+        right = calculate(x-1, y) if x > 1 else 1
+        down = calculate(x, y-1) if y > 1 else 1
+        total = right + down
+        cache[(x, y)] = total
+        return total
+
+result = calculate(20, 20)
+check(result, PROBLEM_NUMBER, ANSWER_HASH)
