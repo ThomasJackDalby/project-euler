8e1150465544ec09b0043d43fb13852699884a10
Tom Dalby <thomasjackdalby@gmail.com>
2021-06-28 23:15:59 +0100

    19

--- 019.py
+++ 019.py
@@ -26,4 +26,52 @@ from common import check
 PROBLEM_NUMBER = 19
 ANSWER_HASH = "a4a042cf4fd6bfb47701cbc8a1653ada"
 
-check(None, PROBLEM_NUMBER, ANSWER_HASH)
+WEEKDAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+class Date:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+def get_days_in_month(month, year):
+    if month in [4, 6, 9, 11]:
+        return 30
+    elif month == 2:
+        if year % 4 == 0:
+            if year % 100 == 0:
+                return 28 if year % 400 == 0 else 27
+            return 28
+        return 27
+    else:
+        return 31 
+days = 0
+day = 1
+month = 1
+year = 1900
+total = 0
+days_in_month = get_days_in_month(month, year)
+keep_going = True
+start_counting = False
+while keep_going:
+    weekday = days % 7
+    if not start_counting and day == 1 and month == 1 and year == 1901:
+        start_counting = True
+    elif day == 31 and month == 12 and year == 2000:
+        keep_going = False
+        break
+    day += 1
+    days += 1
+    if day > days_in_month:
+        month += 1
+        day = 1
+        if month > 12:
+            month = 1
+            year += 1
+        days_in_month = get_days_in_month(month, year)
+    
+    if start_counting and weekday == 6 and day == 1:
+        total += 1
+
+print(total)
+check(total, PROBLEM_NUMBER, ANSWER_HASH)
