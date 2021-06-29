13eba274943114e186716d44d3b8a6216b89261d
Tom Dalby <thomasjackdalby@gmail.com>
2021-06-29 13:12:03 +0100

    moved files under src

--- table.py
+++ table.py
@@ -13,14 +13,14 @@ lines = [
 
 current_line = ""
 for i in range(1, NUMBER_OF_PROBLEMS):
-    file_path = f"{i:03d}.py"
+    file_path = f"src/{i:03d}.py"
     add_link = False
     if os.path.exists(file_path):
         with open(file_path, "r", encoding="utf-8") as file:
             file_line = file.readline()
             add_link = file_line.startswith("# COMPLETED")
 
-    current_line += f"| [{i:03d}]({i:03d}.py) " if add_link else f"| {i:03d} "
+    current_line += f"| [{i:03d}]({file_path}) " if add_link else f"| {i:03d} "
     if i % COLUMNS == 0:
         current_line += "|"
         lines.append(current_line)
